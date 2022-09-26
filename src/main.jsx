ec1ca546b351866e0e8f76329da861dec57e5a35
Saad A. Bazaz <saadbazaz@hotmail.com>
2022-09-26 21:55:32 +0500

    deploy new build

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/GrammarlyBot/">
       <App />
     </Router>
   </React.StrictMode>
