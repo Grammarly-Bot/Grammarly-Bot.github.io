2ff5ae8303268c948edacc32882c1a452f5e0162
Saad A. Bazaz <saadbazaz@hotmail.com>
2022-11-08 15:35:45 +0500

    revert

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename="/#">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
